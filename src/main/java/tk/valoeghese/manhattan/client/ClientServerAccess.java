486ef49df8f4059444438f5859d38af9ad2345af
2xsaiko <git@dblsaiko.net>
2020-07-02 13:15:24 +0200

    Formatting

--- src/main/java/tk/valoeghese/manhattan/client/ClientServerAccess.java
+++ src/main/java/tk/valoeghese/manhattan/client/ClientServerAccess.java
@@ -4,9 +4,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
 
 public class ClientServerAccess {
-
     public static MinecraftServer getServer() {
         return MinecraftClient.getInstance().getServer();
     }
-
 }
